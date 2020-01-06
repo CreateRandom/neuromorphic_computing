4af32a733997cc286c85b0abbc308cc91404ad80
CreateRandom <klausmichaellux@googlemail.com>
2020-01-06 17:00:10 +0100

    Added loihi config

--- convert.py
+++ convert.py
@@ -2,8 +2,19 @@ import os
 
 from snntoolbox.utils.utils import import_configparser
 
+loihi_config_dict = {
+    # nWB has a maximum of 8
+    # weightExponent must be 0 anyway
+    'connection_kwargs': {'numWeightBits': 8, 'weightExponent': 0},
+    # vThMant: no idea how to set this, max is 2 ** 17 - 1
+    # biasExp must be 6 anyway
+    'compartment_kwargs': {'vThMant': 2 ** 8, 'biasExp': 6},
+    # no clue how to set this
+    'desired_threshold_to_input_ratio': 1
 
-def convert_model(path_wd, model_name):
+}
+
+def convert_model(path_wd, model_name, simulator='INI'):
     # Store model so SNN Toolbox can find it.
 
     # SNN TOOLBOX CONFIGURATION #
@@ -25,7 +36,7 @@ def convert_model(path_wd, model_name):
     }
 
     config['simulation'] = {
-        'simulator': 'INI',            # Chooses execution backend of SNN toolbox.
+        'simulator': simulator,            # Chooses execution backend of SNN toolbox.
         'duration': 50,                 # Number of time steps to run each sample.
         'num_to_test': 5,               # How many test samples to run.
         'batch_size': 1,                # Batch size for simulation.
@@ -43,6 +54,9 @@ def convert_model(path_wd, model_name):
         #     'v_mem',
         #     'error_t'}
     }
+
+    if simulator is 'loihi':
+        config['loihi'] = loihi_config_dict
 
     # Store config file.
     config_filepath = os.path.join(path_wd, 'config')
