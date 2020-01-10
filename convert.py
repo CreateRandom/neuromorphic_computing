3ae569c30830aed2459f038dd084c980b527cc50
CreateRandom <klausmichaellux@googlemail.com>
2020-01-10 13:15:54 +0100

    Conversion batch script

--- convert.py
+++ convert.py
@@ -1,6 +1,9 @@
 import os
-
+import time
+import shutil
+from snntoolbox.bin.run import main
 from snntoolbox.utils.utils import import_configparser
+import datetime
 
 loihi_config_dict = {
     # nWB has a maximum of 8
@@ -14,7 +17,7 @@ loihi_config_dict = {
 
 }
 
-def convert_model(path_wd, model_name, simulator='INI'):
+def generate_snn_config(path_wd, model_name, simulator='INI'):
     # Store model so SNN Toolbox can find it.
 
     # SNN TOOLBOX CONFIGURATION #
@@ -59,8 +62,40 @@ def convert_model(path_wd, model_name, simulator='INI'):
         config['loihi'] = loihi_config_dict
 
     # Store config file.
-    config_filepath = os.path.join(path_wd, 'config')
+    config_filepath = os.path.join(path_wd, model_name) + '.config'
     with open(config_filepath, 'w') as configfile:
         config.write(configfile)
 
     return config_filepath
+
+def run_all_on_path(in_path):
+    assert os.path.exists(os.path.join(in_path, 'x_norm.npz')), 'Could not find x_norm.npz on path'
+    candidates = []
+    for filename in os.listdir(in_path):
+        if filename.endswith(".h5"):
+            candidates.append(filename)
+
+    print('Found a total of {} candidates.'.format(len(candidates)))
+    durations = []
+    for i, candidate in enumerate(candidates):
+        # strip the extension
+        candidate = candidate.split('.h5')[0]
+        print('Running candidate {}'.format(candidate))
+        config = generate_snn_config(in_path,candidate, simulator='loihi')
+        start_time = time.time()
+        main(config)
+        end_time = time.time()
+        duration = round(end_time - start_time, 2)
+        durations.append(duration)
+        print('Duration for candidate {}: {}'.format(candidate, duration))
+        mean_duration = sum(durations) / len(durations)
+        n_left = len(candidates[i+1:])
+        pred_duration = mean_duration * n_left
+        print('Predicted duration for remaining candidates: {}'.format(datetime.timedelta(seconds=pred_duration)))
+
+        # rename the log dir
+        shutil.move(os.path.join(in_path,'log'), os.path.join(in_path,candidate))
+
+if __name__ == '__main__':
+    nrc_path = '/homes/klux/models'
+    run_all_on_path(nrc_path)
